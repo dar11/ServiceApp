369faf95f52428bd53ab00908b44e30dcab4bd4d
Daniel Arnsberger <daniel.arnsberger@googlemail.com>
2019-07-10 10:46:17 +0200

    Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SwUpdate, SwPush } from '@angular/service-worker';
 
 @Component({
@@ -7,6 +7,8 @@ import { SwUpdate, SwPush } from '@angular/service-worker';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('videoElement') videoElement: any;
+  video: any;
 
   readonly VAPID_PUBLIC_KEY = "BKwgxDYxGJbZh-hyK64eejQ03_LlwZFto_4mm5Y9HhEh1F5-7ep2ZAqhHFEPapvWD2wfNGHpsh0hyxGNFQ5k7u8";
 
@@ -15,6 +17,7 @@ export class AppComponent implements OnInit {
   constructor(private swUpdate: SwUpdate, private swPush: SwPush) { }
 
   ngOnInit() {
+    this.video = this.videoElement.nativeElement;
     if (this.swUpdate.isEnabled) {
       this.swUpdate.available.subscribe(() => {
         if (confirm("New version available. Load New Version?")) {
@@ -24,9 +27,25 @@ export class AppComponent implements OnInit {
     }
   }
 
+  private addPushSubscribe(sub) {
+
+  }
+
   public subscribe() {
-    this.swPush.requestSubscription({
-      serverPublicKey: this.VAPID_PUBLIC_KEY
-    });
+    this.initCamera({video: true, audio:false});
+    // this.swPush.requestSubscription({
+    //   serverPublicKey: this.VAPID_PUBLIC_KEY
+    // })
+    // .then(sub => this.addPushSubscribe(sub).subscribe())
+    // .catch(err => console.error("Could not subscribe to notifications", err));
+  }
+
+  private initCamera(config: any) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia(config).then(stream => {
+        this.video.src = window.URL.createObjectURL(stream);
+        this.video.play();
+      });
+    }
   }
 }
