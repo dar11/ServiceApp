b08adcd7adbeb613acc45ac3f3ca4a7a8e6945fe
Daniel Arnsberger <daniel.arnsberger@googlemail.com>
2019-07-11 18:07:06 +0200

    Deploy

--- server.js
+++ server.js
@@ -14,4 +14,8 @@ res.sendFile(path.join(__dirname+'/dist/ServiceApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+srv = app.listen(process.env.PORT || 8080);
+
+app.use('/peerjs', ExpressPeerServer(srv, {
+    debug: true
+}));
